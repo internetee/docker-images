5faa7c2969613f98e385ba47561de28a38c1e846
Maciej Szlosarczyk <maciej@sosek.net>
2018-06-14 15:38:14 +0300

    Remove unnecessary files

--- registry/docker_dev.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-docker-compose down
-docker-compose build
-docker-compose run registry rake db:setup:all
-docker-compose run registry rake db:migrate
-docker-compose run registry rake db:fixtures:load
