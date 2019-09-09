69e38faf0c69d40834c9e421cf8eb07d2d40b9d1
Georg <teadur@users.noreply.github.com>
2019-09-09 20:55:01 +0300

    Use debian Buster Slim version  (#3)
    * ruby:2.4 move to buster-slim and include push.sh for upload

--- base_images/ruby_2_4/build.sh
+++ base_images/ruby_2_4/build.sh
@@ -1,1 +1,1 @@
-docker build . --no-cache -t internetee/ruby:2.4 -t internetee/ruby:2.4-jessie -t internetee/ruby:2.4.$(date +'%Y.%m.%d')
+docker build . --no-cache -t internetee/ruby:2.4 -t internetee/ruby:2.4-buster -t internetee/ruby:2.4.$(date +'%Y.%m.%d')
