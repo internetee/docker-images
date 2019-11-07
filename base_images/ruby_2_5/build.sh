b173f46074cd80c65ef85e1e2cc4979c8cacb53e
Georg Kahest <georg@kahest.ee>
2019-11-07 18:01:14 +0200

    ruby 2.5 to buster

--- base_images/ruby_2_5/build.sh
+++ base_images/ruby_2_5/build.sh
@@ -1,1 +1,1 @@
-docker build . --no-cache -t internetee/ruby:2.5 -t internetee/ruby:2.5-stretch -t internetee/ruby:2.5.$(date +'%Y.%m.%d')
+docker build . --no-cache -t internetee/ruby:2.5 -t internetee/ruby:2.5-buster -t internetee/ruby:2.5.$(date +'%Y.%m.%d')
---
+++ base_images/ruby_2_6/build.sh
@@ -0,0 +1,1 @@
+docker build . --no-cache -t internetee/ruby:2.6 -t internetee/ruby:2.6-buster -t internetee/ruby:2.6.$(date +'%Y.%m.%d')
