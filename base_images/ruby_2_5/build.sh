320c8815349458d1e0b92e2a10ce7ac86ddf361e
Georg <teadur@users.noreply.github.com>
2020-04-20 12:21:04 +0300

    Ruby minor version update (#8)
    * change openssl.conf: SECLEVEL to 1
    
    * install libxslt1-dev libxml2-dev wkhtmltopdf
    
    * fix env for buster
    
    * Use et_EE.UTF-8 locale
    
    * include en_US.UTF-8 locale
    
    * fix env, update ruby 2.4/2.5/2.6 images

--- base_images/ruby_2_5/build.sh
+++ base_images/ruby_2_5/build.sh
@@ -1,1 +1,1 @@
-docker build . --no-cache -t internetee/ruby:2.5 -t internetee/ruby:2.5-buster -t internetee/ruby:2.5.$(date +'%Y.%m.%d') -t internetee/ruby
+docker build --pull . --no-cache -t internetee/ruby:2.5 -t internetee/ruby:2.5-buster -t internetee/ruby:2.5.$(date +'%Y.%m.%d') -t internetee/ruby
--- base_images/ruby_2_5/push.sh
+++ base_images/ruby_2_5/push.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 docker push internetee/ruby:2.5
 docker push internetee/ruby:2.5-buster
 docker push internetee/ruby:2.5.$(date +'%Y.%m.%d')
