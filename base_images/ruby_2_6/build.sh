a87682f5b43d343b23d536bdc6e80116095e4925
Georg Kahest <georg@tld.ee>
2020-04-20 11:37:28 +0300

    fix env, update ruby 2.4/2.5/2.6 images

--- base_images/ruby_2_6/build.sh
+++ base_images/ruby_2_6/build.sh
@@ -1,1 +1,1 @@
-docker build . --no-cache -t internetee/ruby:2.6 -t internetee/ruby:2.6-buster -t internetee/ruby:2.6.$(date +'%Y.%m.%d')
+docker build --pull . --no-cache -t internetee/ruby:2.6 -t internetee/ruby:2.6-buster -t internetee/ruby:2.6.$(date +'%Y.%m.%d')
