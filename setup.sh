afe8b8016bdb6ed87454104a3e625c229eec6bf8
Georg Kahest <georg@tld.ee>
2020-08-11 05:13:25 +0300

    add setup.sh to ease testing

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+git clone https://github.com/internetee/registry ../registry
+git clone https://github.com/internetee/auction_center ../auction_center
+git clone https://github.com/internetee/whois ../whois
+git clone https://github.com/internetee/rest-whois ../rest-whois
+git clone https://github.com/internetee/epp_proxy ../epp_proxy
