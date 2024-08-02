752970b1e8879bf51a16ec587d8a172f6fda1382
Rait Nigol <rait@tld.ee>
2024-08-02 18:05:00 +0300

    rework ruby_3_2_2 dockerfile for the new auction (Debian 9 to Debian 12). Use ruby.3.2.2-slim-bookworm as base image.

---
+++ base_images/ruby_3_2_2/ruby_3_2_2_new_auction/build.sh
@@ -0,0 +1,1 @@
+docker build --pull . --no-cache -t internetee/ruby:3.2.2-new-auction-node-18-bookworm
---
+++ base_images/ruby_3_2_2/ruby_3_2_2_new_auction/push.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker push internetee/ruby:3.2.2-new-auction-node-18-bookworm
